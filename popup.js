bdd606538bfe46e8a785c17d9c387375e7bca6c2
Leon Watson <lwatson@fossil.com>
2018-07-11 14:25:05 -0500

    Refactored.

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const searchJishoBy = (section) => {
+        const urlBase = `https://jisho.org/search/%23${section}%20`;
+        
         return function (word) {
-            var query = word.selectionText;
-            const urlBase = `https://jisho.org/search/%23${section}%20`;
-            var url = urlBase;
-            url += query
+            const query = word.selectionText;
+            const url = urlBase + query;
             chrome.tabs.query({ url: 'https://jisho.org/*' }, (tabs)=>{
                 if(tabs.length){
                     const tabId = tabs[0].id
@@ -14,17 +14,17 @@ document.addEventListener('DOMContentLoaded', function () {
                     chrome.tabs.create({ url });
 
                 }
-            })
+            });
 
         };
     }
     chrome.contextMenus.create({
-        title: "Lookup '%s' in Jisho in kanji",
+        title: "Lookup '%s' on Jisho by kanji",
         contexts: ["selection"],  // ContextType
         onclick: searchJishoBy('kanji') // A callback function
     });
     chrome.contextMenus.create({
-        title: "Lookup '%s' in Jisho in words",
+        title: "Lookup '%s' on Jisho by word",
         contexts: ["selection"],  // ContextType
         onclick: searchJishoBy('words') // A callback function
     });
